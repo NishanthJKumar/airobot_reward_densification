3c70dd64952b138f9f07b7e9809536bdfd11423e
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-05-03 12:16:59 -0400

    removes quotes from experiment scripts

--- run_experiments_njk.sh
+++ run_experiments_njk.sh
@@ -3,38 +3,38 @@
 
 # Run Reaching Experiments
 echo "Running experiment... outputing here > reach_sac_single_subgoal.txt"
-python main.py --domain "reach" --algorithm "sac" --reward_type "pddl" --pddl_type "single_subgoal" &> reach_sac_single_subgoal.txt
+python main.py --domain reach --algorithm sac --reward_type pddl --pddl_type single_subgoal &> reach_sac_single_subgoal.txt
 
 echo "Running experiment... outputing here > reach_sac_multi_subgoal.txt"
-python main.py --domain "reach" --algorithm "sac" --reward_type "pddl" --pddl_type "multi_subgoal" &> reach_sac_multi_subgoal.txt
+python main.py --domain reach --algorithm sac --reward_type pddl --pddl_type multi_subgoal &> reach_sac_multi_subgoal.txt
 
 echo "Running experiment... outputing here > reach_sac_grid_based.txt"
-python main.py --domain "reach" --algorithm "sac" --reward_type "pddl" --pddl_type "grid_based" &> reach_sac_grid_based.txt
+python main.py --domain reach --algorithm sac --reward_type pddl --pddl_type grid_based &> reach_sac_grid_based.txt
 
 echo "Running experiment... outputing here > reach_ppo_single_subgoal.txt"
-python main.py --domain "reach" --algorithm "ppo" --reward_type "pddl" --pddl_type "single_subgoal" &> reach_ppo_single_subgoal.txt
+python main.py --domain reach --algorithm ppo --reward_type pddl --pddl_type single_subgoal &> reach_ppo_single_subgoal.txt
 
 echo "Running experiment... outputing here > reach_ppo_multi_subgoal.txt"
-python main.py --domain "reach" --algorithm "ppo" --reward_type "pddl" --pddl_type "multi_subgoal" &> reach_ppo_multi_subgoal.txt
+python main.py --domain reach --algorithm ppo --reward_type pddl --pddl_type multi_subgoal &> reach_ppo_multi_subgoal.txt
 
 echo "Running experiment... outputing here > reach_ppo_grid_based.txt"
-python main.py --domain "reach" --algorithm "ppo" --reward_type "pddl" --pddl_type "grid_based" &> reach_ppo_grid_based.txt
+python main.py --domain reach --algorithm ppo --reward_type pddl --pddl_type grid_based &> reach_ppo_grid_based.txt
 
 # Run Pushing Experiments
 echo "Running experiment... outputing here > push_sac_single_subgoal.txt"
-python main.py --domain "push" --algorithm "sac" --reward_type "pddl" --pddl_type "single_subgoal" &> push_sac_single_subgoal.txt
+python main.py --domain push --algorithm sac --reward_type pddl --pddl_type single_subgoal &> push_sac_single_subgoal.txt
 
 echo "Running experiment... outputing here > push_sac_multi_subgoal.txt"
-python main.py --domain "push" --algorithm "sac" --reward_type "pddl" --pddl_type "multi_subgoal" &> push_sac_multi_subgoal.txt
+python main.py --domain push --algorithm sac --reward_type pddl --pddl_type multi_subgoal &> push_sac_multi_subgoal.txt
 
 echo "Running experiment... outputing here > push_sac_grid_based.txt"
-python main.py --domain "push" --algorithm "sac" --reward_type "pddl" --pddl_type "grid_based" &> push_sac_grid_based.txt
+python main.py --domain push --algorithm sac --reward_type pddl --pddl_type grid_based &> push_sac_grid_based.txt
 
 echo "Running experiment... outputing here > push_ppo_single_subgoal.txt"
-python main.py --domain "push" --algorithm "ppo" --reward_type "pddl" --pddl_type "single_subgoal" &> push_ppo_single_subgoal.txt
+python main.py --domain push --algorithm ppo --reward_type pddl --pddl_type single_subgoal &> push_ppo_single_subgoal.txt
 
 echo "Running experiment... outputing here > push_ppo_multi_subgoal.txt"
-python main.py --domain "push" --algorithm "ppo" --reward_type "pddl" --pddl_type "multi_subgoal" &> push_ppo_multi_subgoal.txt
+python main.py --domain push --algorithm ppo --reward_type pddl --pddl_type multi_subgoal &> push_ppo_multi_subgoal.txt
 
 echo "Running experiment... outputing here > push_ppo_grid_based.txt"
-python main.py --domain "push" --algorithm "ppo" --reward_type "pddl" --pddl_type "grid_based" &> push_ppo_grid_based.txt
+python main.py --domain push --algorithm ppo --reward_type pddl --pddl_type grid_based &> push_ppo_grid_based.txt
