7d948f3c72b17f3e1effb9907ff3af61281c82fe
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-05-03 01:41:23 -0400

    minor qol fix to speed up training

--- main.py
+++ main.py
@@ -179,7 +179,7 @@ parser.add_argument('-g', '--granularity', type=int, default=3, help='Number of 
 parser.add_argument('-drs', '--dynamic_shaping', choices=['basic', 'dist'], nargs='?', help='DRS type to use.')
 args = parser.parse_args()
 
-env_kwargs = dict(reward_type = args.reward_type)
+env_kwargs = dict(reward_type = args.reward_type, gui = False)
 if args.domain == 'reach':
     env_name = "URReacher-v1"
     env_kwargs.update(dict(with_obstacle=True))
