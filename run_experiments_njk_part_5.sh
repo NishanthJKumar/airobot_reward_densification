a9ff6c6f41eb952ba9d67b104dbf2d24575f277f
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-05-04 18:33:00 -0400

    includes 6 different experiment scripts

--- run_experiments_njk_part_5.sh
+++ run_experiments_njk_part_5.sh
@@ -4,4 +4,4 @@
 # python main.py --domain push --algorithm ppo --reward_type dense_handcrafted --pddl_type grid_based --granularity 5 --episode_steps 100 &> push_ppo_dense_handcrafted.txt
 
 echo "Running experiment... outputing here > reach_sac_dense_handcrafted.txt"
-python main.py --domain reach --algorithm sac --reward_type dense_handcrafted --pddl_type grid_based --granularity 5 &> reach_sac_dense_handcrafted.txt
+python main.py --domain reach --algorithm sac --reward_type dense_handcrafted --pddl_type grid_based --granularity 5 --episode_steps 100 &> reach_sac_dense_handcrafted.txt
--- run_experiments_njk_part_6.sh
+++ run_experiments_njk_part_6.sh
@@ -4,4 +4,4 @@
 # python main.py --domain reach --algorithm sac --reward_type sparse_handcrafted --pddl_type grid_based --granularity 5 &> reach_sac_sparse_handcrafted.txt
 
 echo "Running experiment... outputing here > push_sac_dense_handcrafted.txt"
-python main.py --domain push --algorithm sac --reward_type dense_handcrafted --pddl_type grid_based --granularity 5 &> push_sac_dense_handcrafted.txt
+python main.py --domain push --algorithm sac --reward_type dense_handcrafted --pddl_type grid_based --granularity 5 --episode_steps 100 &> push_sac_dense_handcrafted.txt
