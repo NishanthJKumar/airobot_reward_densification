82b83de280c39e3eda3a389986f7cd61f0c92902
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-05-04 04:11:22 -0400

    changes experiment scripts into threes

--- run_experiments_njk_part_1.sh
+++ run_experiments_njk_part_1.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# (Reach) x (SAC and PPO) x (3 pddls)
 
-# Run Reaching Experiments
 echo "Running experiment... outputing here > reach_sac_single_subgoal.txt"
 python main.py --domain reach --algorithm sac --reward_type pddl --pddl_type single_subgoal &> reach_sac_single_subgoal.txt
 
@@ -14,8 +12,3 @@ python main.py --domain reach --algorithm sac --reward_type pddl --pddl_type gri
 echo "Running experiment... outputing here > reach_ppo_single_subgoal.txt"
 python main.py --domain reach --algorithm ppo --reward_type pddl --pddl_type single_subgoal &> reach_ppo_single_subgoal.txt
 
-echo "Running experiment... outputing here > reach_ppo_multi_subgoal.txt"
-python main.py --domain reach --algorithm ppo --reward_type pddl --pddl_type multi_subgoal &> reach_ppo_multi_subgoal.txt
-
-echo "Running experiment... outputing here > reach_ppo_grid_based.txt"
-python main.py --domain reach --algorithm ppo --reward_type pddl --pddl_type grid_based &> reach_ppo_grid_based.txt
