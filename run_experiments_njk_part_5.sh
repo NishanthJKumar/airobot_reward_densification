89ea59e22243220648c2e3796ca5dec0cdad9e90
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-05-04 17:38:33 -0400

    mionr exper change

--- run_experiments_njk_part_5.sh
+++ run_experiments_njk_part_5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Running experiment... outputing here > push_ppo_dense_handcrafted.txt"
-python main.py --domain push --algorithm ppo --reward_type dense_handcrafted --pddl_type grid_based --granularity 5 &> push_ppo_dense_handcrafted.txt
+# echo "Running experiment... outputing here > push_ppo_dense_handcrafted.txt"
+# python main.py --domain push --algorithm ppo --reward_type dense_handcrafted --pddl_type grid_based --granularity 5 --episode_steps 100 &> push_ppo_dense_handcrafted.txt
 
 echo "Running experiment... outputing here > reach_sac_dense_handcrafted.txt"
 python main.py --domain reach --algorithm sac --reward_type dense_handcrafted --pddl_type grid_based --granularity 5 &> reach_sac_dense_handcrafted.txt
--- run_experiments_njk_part_6.sh
+++ run_experiments_njk_part_6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Running experiment... outputing here > reach_sac_sparse_handcrafted.txt"
-python main.py --domain reach --algorithm sac --reward_type sparse_handcrafted --pddl_type grid_based --granularity 5 &> reach_sac_sparse_handcrafted.txt
+# echo "Running experiment... outputing here > reach_sac_sparse_handcrafted.txt"
+# python main.py --domain reach --algorithm sac --reward_type sparse_handcrafted --pddl_type grid_based --granularity 5 &> reach_sac_sparse_handcrafted.txt
 
 echo "Running experiment... outputing here > push_sac_dense_handcrafted.txt"
 python main.py --domain push --algorithm sac --reward_type dense_handcrafted --pddl_type grid_based --granularity 5 &> push_sac_dense_handcrafted.txt
