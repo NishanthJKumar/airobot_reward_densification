fde05c325061a2948db47feb06b4a61b313a4d74
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-05-06 10:35:37 -0400

    makes sure dense handcrafted experiments have 100 episode steps

--- experiment_5_dist_ppo.sh
+++ experiment_5_dist_ppo.sh
@@ -1,8 +1,8 @@
 echo "Running experiment... outputing here > reach_ppo_dense.txt"
-python main.py --domain reach --algorithm ppo --reward_type dense_handcrafted --pddl_type single_subgoal &> reach_ppo_dense.txt
+python main.py --domain reach --algorithm ppo --reward_type dense_handcrafted --pddl_type single_subgoal --episode_steps 100 &> reach_ppo_dense.txt
 
 echo "Running experiment... outputing here > push_ppo_dense.txt"
-python main.py --domain push --algorithm ppo --reward_type dense_handcrafted --pddl_type single_subgoal &> push_ppo_dense.txt
+python main.py --domain push --algorithm ppo --reward_type dense_handcrafted --pddl_type single_subgoal --episode_steps 100 &> push_ppo_dense.txt
 
 echo "Running experiment... outputing here > reach_ppo_single_subgoal_dist.txt"
 python main.py --domain reach --algorithm ppo --reward_type pddl --pddl_type single_subgoal --dynamic_shaping dist &> reach_ppo_single_subgoal_dist.txt
--- experiment_6_dist_sac.sh
+++ experiment_6_dist_sac.sh
@@ -1,8 +1,8 @@
 echo "Running experiment... outputing here > reach_sac_dense.txt"
-python main.py --domain reach --algorithm sac --reward_type dense_handcrafted --pddl_type single_subgoal &> reach_sac_dense.txt
+python main.py --domain reach --algorithm sac --reward_type dense_handcrafted --pddl_type single_subgoal --episode_steps 100 &> reach_sac_dense.txt
 
 echo "Running experiment... outputing here > push_sac_dense.txt"
-python main.py --domain push --algorithm sac --reward_type dense_handcrafted --pddl_type single_subgoal &> push_sac_dense.txt
+python main.py --domain push --algorithm sac --reward_type dense_handcrafted --pddl_type single_subgoal --episode_steps 100 &> push_sac_dense.txt
 
 echo "Running experiment... outputing here > reach_sac_single_subgoal_dist.txt"
 python main.py --domain reach --algorithm sac --reward_type pddl --pddl_type single_subgoal --dynamic_shaping dist &> reach_sac_single_subgoal_dist.txt
