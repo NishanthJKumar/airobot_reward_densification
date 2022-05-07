dff29f1b21f60b4be7dbbdae7848745bdd476086
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-05-07 12:22:40 -0400

    turn off gui in main

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ parser.add_argument('-g', '--granularity', type=int, default=5, help='Number of 
 parser.add_argument('-drs', '--dynamic_shaping', choices=['basic', 'dist'], nargs='?', help='DRS type to use.')
 args = parser.parse_args()
 
-env_kwargs = dict(reward_type = args.reward_type, gui = True)
+env_kwargs = dict(reward_type = args.reward_type, gui = False)
 if args.domain == 'reach':
     env_name = "URReacher-v1"
     env_kwargs.update(dict(with_obstacle=True))
